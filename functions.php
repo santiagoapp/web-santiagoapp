dfb0de263f546d99ce3e269151bf74c9d2890b6d
Santiago Pereira <santiagoapp@gmail.com>
2020-05-11 20:37:12 -0500

    test

--- functions.php
+++ functions.php
@@ -201,9 +201,13 @@ add_action( 'widgets_init', 'minimal_portfolio_widgets_init' );
  * Enqueue scripts and styles.
  */
 function minimal_portfolio_scripts() {
-	echo(is_page_template( 'single-resume' ));
+	
+	global $post;
+	$post_slug=$post->post_name;
+		
+	var_dump(post_slug);
+	
 	if ( is_page_template( 'single-resume' ) ) {
-    var_dump("Hola");
   } else {
     /** Call regular enqueue */
 	}
