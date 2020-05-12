251b8b878c6b3a98b54bf051f08bc3aa58f05c94
Santiago Pereira <santiagoapp@gmail.com>
2020-05-11 21:10:35 -0500

    Header modified.

--- header-resume.php
+++ header-resume.php
@@ -901,20 +901,7 @@
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
   
   
-    <script src="assets/resume/js/jquery.min.js"></script>
-    <script src="assets/resume/js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="assets/resume/js/popper.min.js"></script>
-    <script src="assets/resume/js/bootstrap.min.js"></script>
-    <script src="assets/resume/js/jquery.easing.1.3.js"></script>
-    <script src="assets/resume/js/jquery.waypoints.min.js"></script>
-    <script src="assets/resume/js/jquery.stellar.min.js"></script>
-    <script src="assets/resume/js/owl.carousel.min.js"></script>
-    <script src="assets/resume/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/resume/js/aos.js"></script>
-    <script src="assets/resume/js/jquery.animateNumber.min.js"></script>
-    <script src="assets/resume/js/scrollax.min.js"></script>
-    
-    <script src="js/main.js"></script>
+    <?php wp_footer(); ?>
       
     </body>
   </html>
